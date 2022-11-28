d11a666558fb638d3dfbe8d91ece93ffa39ba515
German Montalbetti <germontalbetti@hotmail.com>
2022-11-28 13:59:21 -0300

    [NEW] - Desafio 12 - Backend - 28-11-2022

---
+++ options/config.js
@@ -0,0 +1,28 @@
+const options = {
+    mysql: {
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'root',
+      password : '',
+      database : 'products'
+    }
+
+  },
+  filePath: {
+    path: './DB/productos.json',
+    pathMsg: './DB/messages.json'
+  },
+  sqlite: {
+    client: 'sqlite3',
+    connection: {
+    filePath: './DB/messages.json'
+    },
+    useNullAsDefault: true
+  },
+  HOST: process.env.HOST || 'localhost'
+}
+  module.exports = {
+    options
+  }
